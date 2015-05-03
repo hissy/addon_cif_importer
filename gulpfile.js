de8b47a1cf928a7089b04865699a7963d7f46faf
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-05-04 06:48:50 +0900

    Update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('zip', function () {
-    return gulp.src(['cif_importer/*','cif_importer/**/*'], {base: "./cif_importer"})
+    return gulp.src(['cif_importer/**/*'], {base: "."})
         .pipe(zip('cif_importer.zip'))
         .pipe(gulp.dest('./build'));
 });
